4da18a7ffb2170b18672330d7359dd62bbcf81f5
RohitAlp <rohit.chavan@alphonsol.com>
2026-02-13 12:48:57 +0530

    change

---
+++ lib/controller/receptionist_controller.dart
@@ -0,0 +1,15 @@
+
+import 'dart:async';
+import 'package:dio/dio.dart';
+
+
+
+// class ReceptionistController extends DioClient {
+//   // Future<Response?> getTestDetails(String token) async {
+//   //   return await requestGET(
+//   //     path: "/app/test-master",
+//   //     token: token,
+//   //   );
+//   // }
+//
+// }
