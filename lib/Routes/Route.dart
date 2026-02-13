b89b54baec5deae53f77d5ef33eb2b984e807371
RohitAlp <rohit.chavan@alphonsol.com>
2026-02-13 11:47:24 +0530

    Routes and splash screen

---
+++ lib/Routes/Route.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../splash_screen/view/SplashScreen.dart';
+import 'RouteName.dart';
+
+class Routes{
+static Route<dynamic> generateRoute(RouteSettings setting){
+  switch (setting.name){
+    case RouteName.splashScreen:
+      return  MaterialPageRoute(builder: (context) => Splashscreen(),);
+
+    default:
+      return MaterialPageRoute(builder: (context) {
+        return Scaffold(
+          body: Text('No Route Found'),
+        );
+      }
+      );
+  }
+}
+}
---
+++ lib/splash_screen/SplashService.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../Routes/RouteName.dart';
+
+
+
+class SplashService {
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+  void isLogin(BuildContext context) async {
+    Timer(
+      const Duration(seconds: 3),
+          () {
+        // Navigator.pushNamedAndRemoveUntil(
+        //   context,
+        //   RouteName.loginScreen,
+        //       (route) => false,
+        // );
+        }
+    );
+  }
+}
