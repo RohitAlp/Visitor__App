4a84c3c5e97aaa641808731d3f124269c8a3bbb7
RohitAlp <rohit.chavan@alphonsol.com>
2026-02-17 18:04:12 +0530

    widgets

--- lib/config/Routes/RouteName.dart
+++ lib/config/Routes/RouteName.dart
@@ -1,5 +1,4 @@
 class RouteName{
   static const String splashScreen = 'splashScreen';
   static const String loginScreen = 'loginScreen';
-
 }
--- lib/screens/splash_screen/SplashService.dart
+++ lib/screens/splash_screen/SplashService.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../config/Routes/RouteName.dart';
-
-
-
-
 class SplashService {
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
