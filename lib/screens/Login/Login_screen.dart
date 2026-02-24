2bbc2c963940319098128e3199263b248c985474
rutujasuryawanshi-dev <rutuja.suryawanshi@alphonsol.com>
2026-02-24 11:57:25 +0530

    login

--- lib/screens/Login/Login_screen.dart
+++ lib/screens/Login/Login_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../config/Routes/RouteName.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -25,6 +27,33 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             const SizedBox(height: 20),
+            Center(
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    RouteName.dashboardScreen,
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
