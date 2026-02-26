0b615189d8c5e81753ab3c3a6e3a83a5b198ba24
RohitAlp <rohit.chavan@alphonsol.com>
2026-02-26 12:25:52 +0530

    manage users and property screen design

--- lib/config/Routes/Route.dart
+++ lib/config/Routes/Route.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:visitorapp/screens/Login/Login_screen.dart';
-import 'package:visitorapp/screens/society_admin/manage_users_screen.dart';
+import 'package:visitorapp/screens/society_admin/manageUserAndProperty/manage_users_screen.dart';
 
 import '../../screens/dashboard/dashboard_screen.dart';
 import '../../screens/splash_screen/SplashScreen.dart';
@@ -16,25 +16,34 @@ import 'RouteName.dart';
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings setting) {
     switch (setting.name) {
+
       case RouteName.splashScreen:
         return MaterialPageRoute(builder: (context) => Splashscreen());
+
       case RouteName.loginScreen:
         return MaterialPageRoute(builder: (context) => LoginScreen());
+
       case RouteName.settingsScreen:
         return MaterialPageRoute(builder: (context) => SettingsScreen());
+
       case RouteName.dashboardScreen:
         return MaterialPageRoute(builder: (context) => DashboardScreen());
+
       case RouteName.manageUsersSocietyAdmin:
-        return MaterialPageRoute(builder: (context) => ManageUsersScreen());
+        final args = setting.arguments as int;
+        return MaterialPageRoute(
+          builder: (_) => ManageUsersScreen(type: args),
+        );
 
       case RouteName.notificationScreen:
-        return MaterialPageRoute(builder: (context) => const NotificationScreen());
+        return MaterialPageRoute(
+          builder: (context) => const NotificationScreen(),
+        );
 
       default:
         return MaterialPageRoute(
-          builder: (context) {
-            return Scaffold(body: Text('No Route Found'));
-          },
+          builder: (context) =>
+          const Scaffold(body: Center(child: Text('No Route Found'))),
         );
     }
   }
--- lib/screens/society_admin/manageUserAndProperty/manage_users_screen.dart
+++ lib/screens/society_admin/manageUserAndProperty/manage_users_screen.dart
@@ -1,33 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:visitorapp/widgets/custom_app_bar.dart';
 
-class ManageUsersScreen extends StatelessWidget {
-  const ManageUsersScreen({super.key});
+class ManageUsersScreen extends StatefulWidget {
+  final int type;
 
+  const ManageUsersScreen({super.key,required this.type});
+
+  @override
+  State<ManageUsersScreen> createState() => _ManageUsersScreenState();
+}
+
+class _ManageUsersScreenState extends State<ManageUsersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: const CustomAppBar(
-        title: "Manage Users",
-        showBackButton: false,
-      ),
+      appBar: const CustomAppBar(title: "Manage Users", showBackButton: false),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
+          Padding(
             padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
-            child: Text(
-              "Manage Users",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.arrow_back),
+                ),
+                SizedBox(width: 8),
+                if(widget.type == 1)
+                  Text(
+                    "Manage Users",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  )
+                else
+                  Text(
+                    "Manage Property",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+              ],
             ),
           ),
 
-          /// Grid
           Expanded(
             child: GridView.count(
               padding: const EdgeInsets.all(12),
@@ -35,7 +59,8 @@ class ManageUsersScreen extends StatelessWidget {
               crossAxisSpacing: 8,
               mainAxisSpacing: 8,
               childAspectRatio: 1,
-              children: const [
+              children: widget.type == 1
+                  ? const [
                 _UserCard(
                   icon: Icons.people_outline,
                   title: "Flat Owners\n(Residents)",
@@ -52,6 +77,8 @@ class ManageUsersScreen extends StatelessWidget {
                   icon: Icons.build_outlined,
                   title: "Vendors",
                 ),
+              ]
+                  : const [
                 _UserCard(
                   icon: Icons.build_outlined,
                   title: "Manage Wings",
@@ -72,14 +99,12 @@ class ManageUsersScreen extends StatelessWidget {
     );
   }
 }
+
 class _UserCard extends StatelessWidget {
   final IconData icon;
   final String title;
 
-  const _UserCard({
-    required this.icon,
-    required this.title,
-  });
+  const _UserCard({required this.icon, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -108,11 +133,7 @@ class _UserCard extends StatelessWidget {
                 color: const Color(0xffFBE9E0),
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: Icon(
-                icon,
-                color: Colors.deepOrange,
-                size: 26,
-              ),
+              child: Icon(icon, color: Colors.deepOrange, size: 26),
             ),
             const SizedBox(height: 14),
             Text(
