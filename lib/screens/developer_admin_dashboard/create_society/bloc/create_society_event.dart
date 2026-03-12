2880bf85e3293f15d3029f73cc82594ad4a07d89
unknown <bhagyashri.jadhav@alphonsol.com>
2026-03-12 18:23:32 +0530

    initial commit

--- lib/screens/developer_admin_dashboard/create_society/bloc/create_society_event.dart
+++ lib/screens/developer_admin_dashboard/create_society/bloc/create_society_event.dart
@@ -1,9 +1,66 @@
-part of 'create_society_bloc.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class CreateSocietyEvent extends Equatable{
-  const CreateSocietyEvent();
+abstract class SocietyEvent extends Equatable {
+  const SocietyEvent();
 
-  List<Object> get props => [];
+  @override
+  List<Object?> get props => [];
 }
 
+class SocietyNameChanged extends SocietyEvent {
+  final String societyName;
 
+  const SocietyNameChanged(this.societyName);
+
+  @override
+  List<Object?> get props => [societyName];
+}
+
+class RegistrationNumberChanged extends SocietyEvent {
+  final String registrationNumber;
+
+  const RegistrationNumberChanged(this.registrationNumber);
+
+  @override
+  List<Object?> get props => [registrationNumber];
+}
+
+class EstablishedYearChanged extends SocietyEvent {
+  final String year;
+
+  const EstablishedYearChanged(this.year);
+
+  @override
+  List<Object?> get props => [year];
+}
+
+class SaveSocietyPressed extends SocietyEvent {}
+class StreetAddressChanged extends SocietyEvent {
+  final String streetAddress;
+  StreetAddressChanged(this.streetAddress);
+}
+
+class LandmarkChanged extends SocietyEvent {
+  final String landmark;
+  LandmarkChanged(this.landmark);
+}
+
+class CityChanged extends SocietyEvent {
+  final String city;
+  CityChanged(this.city);
+}
+
+class StateChanged extends SocietyEvent {
+  final String state;
+  StateChanged(this.state);
+}
+
+class PincodeChanged extends SocietyEvent {
+  final String pincode;
+  PincodeChanged(this.pincode);
+}
+
+class CountryChanged extends SocietyEvent {
+  final String country;
+  CountryChanged(this.country);
+}
--- lib/screens/developer_admin_dashboard/create_society/bloc/create_society_state.dart
+++ lib/screens/developer_admin_dashboard/create_society/bloc/create_society_state.dart
@@ -1,20 +1,80 @@
-part of 'create_society_bloc.dart';
+import 'package:equatable/equatable.dart';
 
-class CreateSocietyState extends Equatable {
-  final String? errorMessage;
-  final Status status;
+class SocietyState extends Equatable {
 
-  const CreateSocietyState({this.errorMessage, this.status = Status.initial});
+  final String societyName;
+  final String registrationNumber;
+  final String establishedYear;
 
-  CreateSocietyState copyWith({
-    final String? errorMessage,
+  final String streetAddress;
+  final String landmark;
+  final String city;
+  final String state;
+  final String pincode;
+  final String country;
 
-  }){
-    return CreateSocietyState(
-      errorMessage: errorMessage,
+  final bool isSubmitting;
+  final bool isSuccess;
+  final bool isFailure;
+
+  const SocietyState({
+    this.societyName = '',
+    this.registrationNumber = '',
+    this.establishedYear = '',
+    this.streetAddress = '',
+    this.landmark = '',
+    this.city = '',
+    this.state = '',
+    this.pincode = '',
+    this.country = 'India',
+    this.isSubmitting = false,
+    this.isSuccess = false,
+    this.isFailure = false,
+  });
+
+  SocietyState copyWith({
+    String? societyName,
+    String? registrationNumber,
+    String? establishedYear,
+    String? streetAddress,
+    String? landmark,
+    String? city,
+    String? state,
+    String? pincode,
+    String? country,
+    bool? isSubmitting,
+    bool? isSuccess,
+    bool? isFailure,
+  }) {
+    return SocietyState(
+      societyName: societyName ?? this.societyName,
+      registrationNumber: registrationNumber ?? this.registrationNumber,
+      establishedYear: establishedYear ?? this.establishedYear,
+      streetAddress: streetAddress ?? this.streetAddress,
+      landmark: landmark ?? this.landmark,
+      city: city ?? this.city,
+      state: state ?? this.state,
+      pincode: pincode ?? this.pincode,
+      country: country ?? this.country,
+      isSubmitting: isSubmitting ?? this.isSubmitting,
+      isSuccess: isSuccess ?? this.isSuccess,
+      isFailure: isFailure ?? this.isFailure,
     );
   }
+
   @override
-  // TODO: implement props
-  List<Object?> get props => [errorMessage,status];
+  List<Object?> get props => [
+    societyName,
+    registrationNumber,
+    establishedYear,
+    streetAddress,
+    landmark,
+    city,
+    state,
+    pincode,
+    country,
+    isSubmitting,
+    isSuccess,
+    isFailure,
+  ];
 }
